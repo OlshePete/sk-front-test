96e9c53003b594cfeb10e3b13166ce3e2a40d50e
OlshePete <olshevskii.pv@gmail.com>
2024-01-20 12:08:49 +0300

    add import redux-devtools deps

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers';
 import rootSaga from './sagas';
 import Messenger from './components/Messenger';
@@ -10,9 +11,8 @@ import './index.css';
 
 try {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+  const store = createStore(reducers, composeWithDevTools(applyMiddleware(sagaMiddleware)));
   sagaMiddleware.run(rootSaga);
-  
   ReactDOM.render(
     <Provider store={store}>
       <Messenger />
